7adcf805dc5e3e9f613f35d2dcb236a55ab36f9e
avera6756 <avera6756@utm.edu.ec>
2022-01-08 20:32:13 -0500

    [ADD] Animation fadeIn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //styles
-import { GlobalStyle } from "./GlobalStyles";
+import { GlobalStyle } from "./Styles/GlobalStyles";
 
 import { ListOfCategories } from './Components/ListOfCategories';
 import { ListOfPhotoCards } from './Components/ListOfPhotoCards';
