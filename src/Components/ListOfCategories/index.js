66914d9660405b4a0a25fdd0f60b45178ecb3649
avera6756 <avera6756@utm.edu.ec>
2022-01-09 10:06:37 -0500

    [UPDATE] UseEffect onScroll ListOfCategories

--- src/Components/ListOfCategories/index.js
+++ src/Components/ListOfCategories/index.js
@@ -7,6 +7,9 @@ import { Category } from '../Category';
 export const ListOfCategories = () => {
     const [categories, setCategories] = useState([]);
 
+    const [showFixed, setShowFixed] = useState(false);
+
+    //Show Categories fetching the data
     useEffect(function(){
         window.fetch('https://petgram-server-alejandroverita-alejandroverita.vercel.app/categories')
             .then(res => res.json())
@@ -15,9 +18,21 @@ export const ListOfCategories = () => {
         })
     }, [])
 
-    
-    return (
-        <List>
+    //Show categories when scroll is higher 200px
+    useEffect(()=>{
+        const onScroll = e => {
+            const newShowFixed = window.scrollY > 200
+            showFixed !== newShowFixed && setShowFixed(newShowFixed)
+        }
+
+        document.addEventListener('scroll', onScroll)
+
+        //Avoid memoryLeak
+        return () => document.removeEventListener('scroll', onScroll)
+    }, [showFixed])
+
+    const renderList = (fixed) => (
+        <List className={fixed ? 'fixed' : ''}>
             {
                 categories.map(category => 
                     <Item key = {category.id}>
@@ -26,5 +41,12 @@ export const ListOfCategories = () => {
                 )
             }
         </List>
+    )
+    
+    return (
+        <>
+            {renderList()}
+            {showFixed && renderList(true)}
+        </>
     )
 }
--- src/Components/ListOfCategories/styles.js
+++ src/Components/ListOfCategories/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {scale} from '../../Styles/animation';
 
 export const List = styled.ul`
     display: flex;
@@ -7,6 +8,21 @@ export const List = styled.ul`
     //desaparece el scroll sin perder la funcionalidad 
     &::-webkit-scrollbar {
         display: none;
+    };
+    &.fixed {
+        background-color: white;
+        border-radius: 60px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+        left: 0;
+        margin: 0 auto;
+        max-width: 400px;
+        padding: 5px;
+        position: fixed;
+        right: 0;
+        top: -20px;
+        transform: scale(0.5);
+        ${scale({time:'0.3s'})}
+        z-index: 1;
     }
 
   `
