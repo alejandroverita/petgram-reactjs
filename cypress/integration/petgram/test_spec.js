138502720167c1faabd04e68ccffee182321a23d
avera6756 <avera6756@utm.edu.ec>
2022-01-16 14:40:00 -0500

    [ADD] Testin with Cypress and Sw with Workbox

---
+++ cypress/integration/petgram/test_spec.js
@@ -0,0 +1,20 @@
+/* global describe, it, cy*/
+
+describe('Petgram', function(){
+    it('Para ver si funciona', function () {
+        cy.visit('/')
+    })
+    it('navegamos a una categoria y vemos fotos', function () {
+        cy.visit('/pet/1')
+        cy.get('article')
+    })
+    it('si podemos navegar con la navbar a la home', function () {
+        cy.visit('/pet/1')
+        cy.get('nav a').first().click()
+        cy.url().should('include', '/')
+    })
+    it('si los usuarios no regitrados ven el formulario de registro e inicio de sesion al ir a favs', function () {
+        cy.visit('/favs')
+        cy.get('form').should('have.length', 2)
+    })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,58 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
 
 module.exports = {
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
     publicPath: '/'
   },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'src/index.html',
     }),
     new WebpackPwaManifestPlugin({
+      filename: 'manifest.webmanifest',
       name: 'Petgram - Your pet photos app',
       shortname: 'Petgram -🐱',
       description: 'Find pet photos and use it as wallpaper',
       background_color: '#fff',
       theme_color: '#8d00ff',
+      prefer_related_applications: true,
       icons: [
         {
           src: path.resolve('src/Assets/icon.png'),
           sizes: [96, 128, 192, 256, 384, 512],
-          purpose: 'maskable'
+          purpose: 'maskable any',
         }
       ]
-    })
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      // maximumFileSizeToCacheInBytes: 5000000,
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images',
+          }
+        },
+        {
+          urlPattern: new RegExp('https://vercel.com/alejandroverita/petgram-server-alejandroverita/7XSUtLDHMkxBWPnto3J12Efs2bs8'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api',
+          }
+        }
+      ]
+    }),
   ],
   module: {
     rules: [
@@ -41,7 +68,7 @@ module.exports = {
             ]
           }
         }
-      }
+      },
     ]
   },
   devServer: {
